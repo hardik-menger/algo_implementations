f3222c4054c1d3ec3ed7706a57045133d844cda9
hardik.menger <hardik.menger@cimpress.com>
2021-04-08 11:41:01 +0530

    done

---  RotateLinkedList.java
+++  RotateLinkedList.java
@@ -18,6 +18,8 @@ class ListNode {
 }
 
 class RotateLinkedList {
+    int k;
+    ListNode root;
 
     public ListNode rotateRight(ListNode head, int k) {
         ListNode temp = head;
@@ -65,6 +67,34 @@ class RotateLinkedList {
         return temp;
     }
 
+    private ListNode reAttachRecursive(ListNode head, int k) {
+        ListNode temp = head;
+        int length = getLength(temp);
+        if (length <= 1)
+            return head;
+        int tries = k % length;
+        this.k = tries;
+        this.root = head;
+        solver(head);
+        return root;
+    }
+
+    void solver(ListNode head) {
+        ListNode temp = head;
+        if (temp.next != null) {
+            solver(temp.next);
+        }
+        if (temp.next != null && temp.next.next == null && k-- != 0) {
+            ListNode prev = temp;
+            ListNode last = temp.next;
+            prev.next = null;
+            last.next = root;
+            root = last;
+
+        } else
+            return;
+    }
+
     public static void main(String[] args) {
         RotateLinkedList rotateLinkedList = new RotateLinkedList();
         ListNode root = new ListNode(1);
@@ -73,5 +103,6 @@ class RotateLinkedList {
         root.next.next.next = new ListNode(4);
         root.next.next.next.next = new ListNode(5);
         rotateLinkedList.print(rotateLinkedList.rotateRight(root, 50000));
+        rotateLinkedList.print(rotateLinkedList.reAttachRecursive(root, 50000));
     }
 }
