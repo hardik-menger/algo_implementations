c88696de4e76d4c6012c37a8f4a13e17f59e0402
hardik.menger <hardik.menger@cimpress.com>
2021-04-09 01:44:42 +0530

    done

---  MatrixPathSum.java
+++  MatrixPathSum.java
@@ -1,14 +1,35 @@
+import java.util.*;
+
 class MatrixPathSum {
+
     public int minPathSum(int[][] grid) {
-        return solve(grid, grid.length - 1, grid[0].length - 1);
+        if (grid.length == 1 && grid[0].length == 1)
+            return grid[0][0];
+        int[][] memo = new int[grid.length][grid[0].length];
+        for (int[] row : memo)
+            Arrays.fill(row, Integer.MAX_VALUE);
+        solve(grid, memo, 0, 0);
+        return memo[0][0];
     }
 
-    private int solve(int[][] grid, int i, int j) {
-        if (i < 0 || j < 0)
-            return 0;
-        System.out.println(i + "  " + j);
-        int left = solve(grid, i - 1, j);
-        int up = solve(grid, i, j - 1);
-        return Math.min(left, up) + grid[i][j];
+    private int solve(int[][] grid, int[][] memo, int i, int j) {
+        if (i > grid.length - 1 || j > grid[0].length - 1)
+            return Integer.MAX_VALUE;
+
+        if (memo[i][j] != Integer.MAX_VALUE)
+            return memo[i][j];
+        if (i == grid.length - 1 && j == grid[0].length - 1)
+            return grid[i][j];
+        else {
+            int adder = Math.min(solve(grid, memo, i + 1, j), solve(grid, memo, i, j + 1));
+            memo[i][j] = grid[i][j] + adder;
+            return memo[i][j];
+        }
+    }
+
+    public static void main(String args[]) {
+        MatrixPathSum mMatrixPathSum = new MatrixPathSum();
+        int res[][] = { { 1, 2, 5 }, { 3, 2, 1 } };
+        System.out.println(mMatrixPathSum.minPathSum(res));
     }
 }
