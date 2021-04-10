b1efff09798afcd104da38137e12e19f4fd96976
hardik.menger <hardik.menger@cimpress.com>
2021-04-11 00:45:00 +0530

    DONE

--- 98. Validate Binary Search Tree.java
+++ 98. Validate Binary Search Tree.java
@@ -23,11 +23,25 @@ class ValidateBinarySearchTree {
 
     int current = Integer.MIN_VALUE;
 
-    public boolean isValidBST(TreeNode root) {
+    public boolean isValidBST1(TreeNode root) {
         ArrayList<Integer> al = new ArrayList<Integer>();
         solve(root, al);
         System.out.println(al);
         return isSorted(al);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return solve2(root, root.left, root.right);
+    }
+
+    private boolean solve2(TreeNode root, TreeNode left, TreeNode right) {
+        if (root == null)
+            return true;
+        if (left != null && left.val >= root.val)
+            return false;
+        if (right != null && right.val <= root.val)
+            return false;
+        return solve2(root.left, left, root) && solve2(root.right, root, right);
     }
 
     private boolean isSorted(ArrayList<Integer> al) {
