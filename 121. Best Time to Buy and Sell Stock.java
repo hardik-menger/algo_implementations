aa72dc6a760214458259758f21ddeea22eb64535
hardik.menger <hardik.menger@cimpress.com>
2021-04-13 20:23:18 +0530

    done

---
+++ 121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,13 @@
+class BestTimetoBuyandSellStock {
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+
+            maxprofit = Math.max(maxprofit, prices[i] - minprice);
+        }
+        return maxprofit;
+    }
+}
