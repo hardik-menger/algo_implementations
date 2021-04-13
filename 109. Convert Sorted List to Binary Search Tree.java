575a7abf536fe3f6715b195883ea4979f3cf00f0
hardik.menger <hardik.menger@cimpress.com>
2021-04-13 12:05:32 +0530

    done

--- 109. Convert Sorted List to Binary Search Tree.java
+++ 109. Convert Sorted List to Binary Search Tree.java
@@ -45,62 +45,33 @@ class SortedListtoBinarySearchTree {
         }
     }
 
-    public TreeNode sortedListToBST(ListNode head) {
-        ListNode temp = head;
-        return null;
+    private ListNode head;
+
+    public TreeNode sortedListToBST(ListNode root) {
+        int len = getLength(root);
+        this.head = root;
+        return solve(len);
     }
 
-    class Avl {
-        private int height(TreeNode N) {
-            if (N == null)
-                return 0;
-            return N.height;
-        }
-
-        TreeNode insert(TreeNode node, int val) {
-            if (node == null)
-                return new TreeNode(val);
-            if (node.val > val)
-                node.left = insert(node.left, val);
-            else
-                node.right = insert(node.right, val);
-            node.height = Math.max(node.right.height, node.left.height) + 1;
-            int balance = getBalance(node);
-            // Left Left Case
-            if (balance > 1 && val < node.left.val)
-                return rightRotate(node);
-
-            // Right Right Case
-            if (balance < -1 && val > node.right.val)
-                return leftRotate(node);
-
-            // Left Right Case
-            if (balance > 1 && val > node.left.val) {
-                node.left = leftRotate(node.left);
-                return rightRotate(node);
-            }
-
-            // Right Left Case
-            if (balance < -1 && val < node.right.val) {
-                node.right = rightRotate(node.right);
-                return leftRotate(node);
-            }
-            return node;
-        }
-
-        private SortedListtoBinarySearchTree.TreeNode leftRotate(SortedListtoBinarySearchTree.TreeNode node) {
+    private TreeNode solve(int length) {
+        if (length <= 0)
             return null;
-        }
-
-        private SortedListtoBinarySearchTree.TreeNode rightRotate(SortedListtoBinarySearchTree.TreeNode node) {
-            return null;
-        }
-
-        private int getBalance(TreeNode node) {
-            if (node == null)
-                return 0;
-            else
-                return height(node.left) - height(node.right);
-        }
+        TreeNode left = solve(length / 2);
+        TreeNode root = new TreeNode(head.val);
+        root.left = left;
+        this.head = this.head.next;
+        root.right = solve(length - 1 - length / 2);
+        return root;
     }
+
+    private int getLength(ListNode root) {
+        int c = 0;
+        ListNode temp = root;
+        while (temp != null) {
+            temp = temp.next;
+            c = c + 1;
+        }
+        return c;
+    }
+
 }
