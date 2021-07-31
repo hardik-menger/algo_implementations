4faad495f3e2f12937755d58fa67a3235511b8c5
hardik <hardik.menger@cimpress.com>
2021-08-01 02:41:14 +0530

    done

--- 84. Largest Rectangle in Histogram.java
+++ 84. Largest Rectangle in Histogram.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 //https://www.youtube.com/watch?v=zx5Sw9130L0
 class LargestRectangleiHistogram {
-    public int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea1(int[] heights) {
         class Pair<T, Q> {
             T p1;
             Q p2;
@@ -31,5 +31,31 @@ class LargestRectangleiHistogram {
         }
         return maxarea;
 
+    }
+
+    public int largestRectangleArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        int[] lessFromLeft = new int[height.length], lessFromRight = new int[height.length];
+        lessFromRight[height.length - 1] = height.length;
+        lessFromLeft[0] = -1;
+        for (int i = 1; i < height.length; i++) {
+            int p = i - 1;
+            while (p >= 0 && height[p] >= height[i])
+                p = lessFromLeft[p];
+            lessFromLeft[i] = p;
+        }
+        for (int i = height.length - 2; i >= 0; i--) {
+            int p = i + 1;
+            while (p < height.length && height[p] >= height[i])
+                p = lessFromRight[p];
+            lessFromRight[i] = p;
+        }
+        for (int i = 0; i < height.length; i++) {
+            maxArea = Math.max(maxArea, height[i] * (lessFromRight[i] - lessFromLeft[i] - 1));
+        }
+        return maxArea;
     }
 }
