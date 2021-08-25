45d39c98170a123be3b8e2c473271c89aaa455ea
hardik-menger <hardik.menger@cimpress.com>
2021-08-25 21:52:42 +0530

    done

---
+++ 110. Balanced Binary Tree.java
@@ -0,0 +1,41 @@
+class BalancedBinaryTree {
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return helper(root) != -1;
+    }
+
+    private int helper(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = helper(root.left);
+        if (left == -1)
+            return -1;
+        int right = helper(root.right);
+        if (right == -1)
+            return -1;
+        if (Math.abs(left - right) > 1)
+            return -1;
+        else
+            return Math.max(left, right) + 1;
+    }
+}
