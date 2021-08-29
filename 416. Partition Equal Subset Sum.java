f0c1c76975ed83951c8c707a51f075f22d59191d
hardik-menger <hardik.menger@cimpress.com>
2021-08-29 16:59:28 +0530

    done

---
+++ 416. Partition Equal Subset Sum.java
@@ -0,0 +1,24 @@
+class PartitionEqualSubsetSum {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums)
+            sum += num;
+        if ((sum & 1) > 0)
+            return false;
+        sum /= 2;
+        int n = nums.length;
+        boolean dp[][] = new boolean[nums.length + 1][sum + 1];
+        dp[0][0] = true;
+        for (int i = 1; i < n + 1; i++)
+            dp[i][0] = true;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (j >= nums[i - 1])
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][sum];
+    }
+}
