d06c779ba9344563ab8c1533ed2a897103af2a5b
hardik-menger <hardik.menger@cimpress.com>
2021-11-20 01:24:49 +0530

    done

---
+++ CSES/RemovalGame.java
@@ -0,0 +1,132 @@
+import java.util.*;
+import java.io.*;
+
+class RemovalGame {
+    // SOLUTION BEGIN
+    void pre() throws Exception {
+
+    }
+
+    void solve(int TC) throws Exception {
+        int n = ni();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = ni();
+        long dp[][] = new long[n][n];
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+                long p = i + 2 <= j ? dp[i + 2][j] : 0;
+                long q = i + 1 <= j - 1 ? dp[i + 1][j - 1] : 0;
+                long r = j - 2 >= i ? dp[i][j - 2] : 0;
+                dp[i][j] = Math.max(a[i] + Math.min(p, q), a[j] + Math.min(q, r));
+            }
+        }
+        pn(dp[0][n - 1]);
+    }
+
+    static boolean multipleTC = false;
+    static FastReader in;
+    static PrintWriter out;
+    static Scanner sc;
+
+    void run() throws Exception {
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        sc = new Scanner(System.in);
+        int T = (multipleTC) ? ni() : 1;
+        pre();
+        for (int t = 1; t <= T; t++)
+            solve(t);
+        out.flush();
+        out.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            new RemovalGame().run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    int bit(long n) {
+        return (n == 0) ? 0 : (1 + bit(n & (n - 1)));
+    }
+
+    void p(Object o) {
+        out.print(o);
+    }
+
+    void pn(Object o) {
+        out.println(o);
+    }
+
+    void pni(Object o) {
+        out.println(o);
+        out.flush();
+    }
+
+    String n() throws Exception {
+        return in.next();
+    }
+
+    String nln() throws Exception {
+        return in.nextLine();
+    }
+
+    int ni() throws Exception {
+        return in.nextInt();
+    }
+
+    long nl() throws Exception {
+        return in.nextLong();
+    }
+
+    double nd() throws Exception {
+        return in.nextDouble();
+    }
+
+}
+
+class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
