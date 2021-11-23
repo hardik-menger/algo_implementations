163cadb5e319ff28ee579d8ac4676c1a393b2c3a
hardik-menger <hardik.menger@cimpress.com>
2021-11-23 11:07:13 +0530

    done

---
+++ Utils/PrimeSeive.java
@@ -0,0 +1,138 @@
+import java.util.*;
+import java.io.*;
+
+class PrimeSeive {
+    // SOLUTION BEGIN
+    void pre() throws Exception {
+
+    }
+
+    int n = 1000000;
+
+    void solve(int TC) throws Exception {
+        int p = ni();
+        boolean isprime[] = new boolean[n];
+        Arrays.fill(isprime, true);
+        isprime[0] = isprime[1] = true;
+        for (int i = 2; i * i < n; i++) {
+            if (isprime[i])
+                for (int j = i; i * j < n; j++) {
+                    isprime[i * j] = false;
+                }
+        }
+        pn(isprime[p]);
+    }
+
+    int gcd(int a, int b) {
+        if (b > a)
+            return gcd(b, a);
+        if (b == 0)
+            return a;
+        return gcd(b, b % a);
+    }
+
+    static boolean multipleTC = false;
+    static FastReader in;
+    static PrintWriter out;
+    static Scanner sc;
+
+    void run() throws Exception {
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        sc = new Scanner(System.in);
+        int T = (multipleTC) ? ni() : 1;
+        pre();
+        for (int t = 1; t <= T; t++)
+            solve(t);
+        out.flush();
+        out.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            new PrimeSeive().run();
+        } catch (Exception e) {
+
+        }
+    }
+
+    int bit(long n) {
+        return (n == 0) ? 0 : (1 + bit(n & (n - 1)));
+    }
+
+    void p(Object o) {
+        out.print(o);
+    }
+
+    void pn(Object o) {
+        out.println(o);
+    }
+
+    void pni(Object o) {
+        out.println(o);
+        out.flush();
+    }
+
+    String n() throws Exception {
+        return in.next();
+    }
+
+    String nln() throws Exception {
+        return in.nextLine();
+    }
+
+    int ni() throws Exception {
+        return in.nextInt();
+    }
+
+    long nl() throws Exception {
+        return in.nextLong();
+    }
+
+    double nd() throws Exception {
+        return in.nextDouble();
+    }
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
