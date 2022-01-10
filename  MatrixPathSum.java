9687c91fd84deabb64f506e4a087d1c3aeac1b9c
hardik-menger <hardik.menger@cimpress.com>
2022-01-10 11:45:36 +0530

    done

---  MatrixPathSum.java
+++  MatrixPathSum.java
@@ -49,7 +49,7 @@ class MatrixPathSum {
         return dp[grid.length - 1][grid[0].length - 1];
     }
 
-    public int minPathSum(int[][] grid) {
+    public int minPathSum3(int[][] grid) {
         if (grid.length == 1 && grid[0].length == 1)
             return grid[0][0];
         int dp[][] = new int[grid.length][grid[0].length];
@@ -69,6 +69,28 @@ class MatrixPathSum {
             }
         }
         return dp[0][0];
+    }
+
+    public int minPathSum(int[][] grid) {
+        if (grid.length == 1 && grid[0].length == 1)
+            return grid[0][0];
+        int dp[][] = new int[grid.length][grid[0].length];
+        int sum = 0;
+        for (int i = 0; i < grid.length; i++) {
+            sum += grid[i][0];
+            dp[i][0] = sum;
+        }
+        sum = 0;
+        for (int i = 0; i < grid[0].length; i++) {
+            sum += grid[0][i];
+            dp[0][i] = sum;
+        }
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[i].length; j++) {
+                dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+            }
+        }
+        return dp[grid.length - 1][grid[0].length - 1];
     }
 
     public static void main(String args[]) {
