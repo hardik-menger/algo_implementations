bef7239ebc086b10f724e21a113566c1810e9380
hardik-menger <hardik.menger@cimpress.com>
2022-01-13 12:07:14 +0530

    done

--- 300. Longest Increasing Subsequence.java
+++ 300. Longest Increasing Subsequence.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 class LongestIncreasingSubsequence {
     // recursive
     public int lengthOfLIS1(int[] nums) {
@@ -16,26 +20,24 @@ class LongestIncreasingSubsequence {
 
     // dp
     public int lengthOfLIS2(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
-        }
         int dp[] = new int[nums.length];
         dp[0] = 1;
-        int maxVal = 1;
+        int res = 1;
         for (int i = 1; i < nums.length; i++) {
-            int tempVal = 0;
+            int temp = 0;
             for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i])
-                    tempVal = Math.max(tempVal, dp[j]);
+                if (nums[i] > nums[j])
+                    temp = Math.max(temp, dp[j]);
             }
-            dp[i] = tempVal + 1;
-            maxVal = Math.max(maxVal, dp[i]);
+            dp[i] = temp + 1;
+
+            res = Math.max(res, dp[i]);
         }
-        return maxVal;
+        return res;
     }
 
     // binarysearch
-    public int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS3(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
@@ -53,5 +55,26 @@ class LongestIncreasingSubsequence {
             }
         }
         return length;
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        ArrayList<Integer> al = new ArrayList<>(nums.length);
+        al.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            int index = Collections.binarySearch(al, nums[i]);
+            if (index < 0)
+                index = ~index;
+            if (index == al.size()) {
+                al.add(nums[i]);
+            } else
+                al.set(index, nums[i]);
+        }
+        return al.size();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestIncreasingSubsequence().lengthOfLIS2(new int[] { 1, 3, 6, 7, 9, 4, 10, 5, 6 }));
+        System.out.println(new LongestIncreasingSubsequence().lengthOfLIS2(new int[] { 0, 1, 0, 3, 2, 3 }));
+        System.out.println(new LongestIncreasingSubsequence().lengthOfLIS2(new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5 }));
     }
 }
