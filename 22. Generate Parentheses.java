fc1c731ade404a055b8bc15d6ed6cb4c554818df
hardik-menger <hardik.menger@cimpress.com>
2022-01-18 01:31:52 +0530

    done

---
+++ 22. Generate Parentheses.java
@@ -0,0 +1,135 @@
+import java.util.*;
+import java.io.*;
+
+class Main22 {
+    // SOLUTION BEGIN
+    void pre() throws Exception {
+
+    }
+
+    void solve(int TC) throws Exception {
+        System.out.println(Arrays.toString(generateParenthesis(3).toArray()));
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<String>();
+        recursive(result, n, 0, "");
+        return result;
+    }
+
+    private void recursive(List<String> result, int canOpen, int canClose, String string) {
+        if (canOpen == 0 && canClose == 0) {
+            result.add(string);
+            return;
+        }
+        if (canOpen > 0)
+            recursive(result, canOpen - 1, canClose + 1, string + "(");
+        if (canClose > 0)
+            recursive(result, canOpen, canClose - 1, string + ")");
+    }
+
+    static boolean multipleTC = false;
+    static FastReader in;
+    static PrintWriter out;
+    static Scanner sc;
+
+    void run() throws Exception {
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        sc = new Scanner(System.in);
+        int T = (multipleTC) ? ni() : 1;
+        pre();
+        for (int t = 1; t <= T; t++)
+            solve(t);
+        out.flush();
+        out.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            new Main22().run();
+        } catch (Exception e) {
+
+        }
+    }
+
+    int bit(long n) {
+        return (n == 0) ? 0 : (1 + bit(n & (n - 1)));
+    }
+
+    void p(Object o) {
+        out.print(o);
+    }
+
+    void pn(Object o) {
+        out.println(o);
+    }
+
+    void pni(Object o) {
+        out.println(o);
+        out.flush();
+    }
+
+    String n() throws Exception {
+        return in.next();
+    }
+
+    String nln() throws Exception {
+        return in.nextLine();
+    }
+
+    int ni() throws Exception {
+        return in.nextInt();
+    }
+
+    long nl() throws Exception {
+        return in.nextLong();
+    }
+
+    double nd() throws Exception {
+        return in.nextDouble();
+    }
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
---
+++ 402. Remove K Digits.java
@@ -0,0 +1,144 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.io.*;
+
+class Main402 {
+    // SOLUTION BEGIN
+    void pre() throws Exception {
+
+    }
+
+    public String removeKdigits(String num, int k) {
+        if (k == 0)
+            return num;
+        Stack<Integer> st = new Stack<Integer>();
+        for (int i = 0; i < num.length(); i++) {
+            while (!st.isEmpty() && k > 0 && num.charAt(i) < st.peek()) {
+                st.pop();
+                k--;
+            }
+            if (!st.isEmpty() || num.charAt(i) != '0')
+                st.push((int) num.charAt(i));
+        }
+        while (!st.isEmpty() && k > 0)
+            st.pop();
+        if (st.isEmpty())
+            return "0";
+        ArrayList<Integer> ch = new ArrayList<Integer>();
+        while (!st.isEmpty()) {
+            ch.add(st.pop());
+        }
+        Collections.reverse(ch);
+        return ch.stream().map(Object::toString)
+                .collect(Collectors.joining(", "));
+    }
+
+    void solve(int TC) throws Exception {
+        int n = ni();
+    }
+
+    static boolean multipleTC = false;
+    static FastReader in;
+    static PrintWriter out;
+    static Scanner sc;
+
+    void run() throws Exception {
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        sc = new Scanner(System.in);
+        int T = (multipleTC) ? ni() : 1;
+        pre();
+        for (int t = 1; t <= T; t++)
+            solve(t);
+        out.flush();
+        out.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            new Main402().run();
+        } catch (Exception e) {
+
+        }
+    }
+
+    int bit(long n) {
+        return (n == 0) ? 0 : (1 + bit(n & (n - 1)));
+    }
+
+    void p(Object o) {
+        out.print(o);
+    }
+
+    void pn(Object o) {
+        out.println(o);
+    }
+
+    void pni(Object o) {
+        out.println(o);
+        out.flush();
+    }
+
+    String n() throws Exception {
+        return in.next();
+    }
+
+    String nln() throws Exception {
+        return in.nextLine();
+    }
+
+    int ni() throws Exception {
+        return in.nextInt();
+    }
+
+    long nl() throws Exception {
+        return in.nextLong();
+    }
+
+    double nd() throws Exception {
+        return in.nextDouble();
+    }
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
