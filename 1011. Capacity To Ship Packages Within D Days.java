d1e6937349848d4ffb9234e5bad440a3cb3f2f01
hardik-menger <hardik.menger@cimpress.com>
2022-02-03 00:15:14 +0530

    done

---
+++ 1011. Capacity To Ship Packages Within D Days.java
@@ -0,0 +1,140 @@
+import java.util.*;
+import java.io.*;
+
+class Main1011 {
+
+    public int shipWithinDays(int[] weights, int D) {
+        int left = Arrays.stream(weights).max().getAsInt();
+        int right = Arrays.stream(weights).sum();
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int localsum = 0, needed = 1;
+            for (int weight : weights) {
+                if (localsum + weight > mid) {
+                    needed++;
+                    localsum = 0;
+                }
+                localsum += weight;
+            }
+            if (needed > D)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return left;
+    }
+
+    // SOLUTION BEGIN
+    void pre() throws Exception {
+
+    }
+
+    void solve(int TC) throws Exception {
+        System.out.println(shipWithinDays(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 5));
+    }
+
+    static boolean multipleTC = false;
+    static FastReader in;
+    static PrintWriter out;
+    static Scanner sc;
+
+    void run() throws Exception {
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        sc = new Scanner(System.in);
+        int T = (multipleTC) ? ni() : 1;
+        pre();
+        for (int t = 1; t <= T; t++)
+            solve(t);
+        out.flush();
+        out.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            new Main1011().run();
+        } catch (Exception e) {
+
+        }
+    }
+
+    int bit(long n) {
+        return (n == 0) ? 0 : (1 + bit(n & (n - 1)));
+    }
+
+    void p(Object o) {
+        out.print(o);
+    }
+
+    void pn(Object o) {
+        out.println(o);
+    }
+
+    void pni(Object o) {
+        out.println(o);
+        out.flush();
+    }
+
+    String n() throws Exception {
+        return in.next();
+    }
+
+    String nln() throws Exception {
+        return in.nextLine();
+    }
+
+    int ni() throws Exception {
+        return in.nextInt();
+    }
+
+    long nl() throws Exception {
+        return in.nextLong();
+    }
+
+    double nd() throws Exception {
+        return in.nextDouble();
+    }
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
---
+++ 438. Find All Anagrams in a String.java
@@ -0,0 +1,153 @@
+import java.util.*;
+import java.io.*;
+
+class Main438 {
+    // SOLUTION BEGIN
+    void pre() throws Exception {
+
+    }
+
+    public List<Integer> findAnagrams(String s, String p) {
+        ArrayList<Integer> soln = new ArrayList<Integer>();
+
+        // Check for bad input
+        if (s.length() == 0 || p.length() == 0 || s.length() < p.length()) {
+            return new ArrayList<Integer>();
+        }
+        int[] chars = new int[26];
+        for (Character c : p.toCharArray())
+            chars[c - 'a']++;
+        int start = 0, end = 0, len = p.length(), diff = len;
+        char temp;
+        for (end = 0; end < len; end++) {
+            temp = s.charAt(end);
+            chars[temp - 'a']--;
+            if (chars[temp - 'a'] >= 0)
+                diff--;
+        }
+        if (diff == 0)
+            soln.add(0);
+        while (end < s.length()) {
+            temp = s.charAt(start++);
+            if (chars[temp - 'a'] >= 0)
+                diff++;
+            chars[temp - 'a']++;
+            temp = s.charAt(end++);
+            chars[temp - 'a']--;
+            if (chars[temp - 'a'] >= 0)
+                diff--;
+            if (diff == 0)
+                soln.add(start);
+        }
+        return soln;
+    }
+
+    void solve(int TC) throws Exception {
+        int n = ni();
+    }
+
+    static boolean multipleTC = false;
+    static FastReader in;
+    static PrintWriter out;
+    static Scanner sc;
+
+    void run() throws Exception {
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        sc = new Scanner(System.in);
+        int T = (multipleTC) ? ni() : 1;
+        pre();
+        for (int t = 1; t <= T; t++)
+            solve(t);
+        out.flush();
+        out.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            new Main438().run();
+        } catch (Exception e) {
+
+        }
+    }
+
+    int bit(long n) {
+        return (n == 0) ? 0 : (1 + bit(n & (n - 1)));
+    }
+
+    void p(Object o) {
+        out.print(o);
+    }
+
+    void pn(Object o) {
+        out.println(o);
+    }
+
+    void pni(Object o) {
+        out.println(o);
+        out.flush();
+    }
+
+    String n() throws Exception {
+        return in.next();
+    }
+
+    String nln() throws Exception {
+        return in.nextLine();
+    }
+
+    int ni() throws Exception {
+        return in.nextInt();
+    }
+
+    long nl() throws Exception {
+        return in.nextLong();
+    }
+
+    double nd() throws Exception {
+        return in.nextDouble();
+    }
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
