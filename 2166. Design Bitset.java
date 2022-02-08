66fc6bb5dc8ce8e444525c7d9075147ccb5157be
hardik-menger <hardik.menger@cimpress.com>
2022-02-08 23:13:36 +0530

    done

--- 2166. Design Bitset.java
+++ 2166. Design Bitset.java
@@ -3,53 +3,55 @@ import java.io.*;
 
 class Main2166 {
 
-    int size;
-    Set<Integer> one = new HashSet<>();
-    Set<Integer> zero = new HashSet<>();
+    class Bitset {
+        int size;
+        Set<Integer> one = new HashSet<>();
+        Set<Integer> zero = new HashSet<>();
 
-    public void Bitset(int size) {
-        this.size = size;
-        for (int i = 0; i < size; i++)
-            zero.add(i);
-    }
-
-    public void fix(int idx) {
-        one.add(idx);
-        zero.remove(idx);
-    }
-
-    public void unfix(int idx) {
-        one.remove(idx);
-        zero.add(idx);
-    }
-
-    public void flip() {
-        Set<Integer> s = one;
-        one = zero;
-        zero = s;
-    }
-
-    public boolean all() {
-        return one.size() == size;
-    }
-
-    public boolean one() {
-        return one.size() >= 1;
-    }
-
-    public int count() {
-        return one.size();
-    }
-
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < size; i++) {
-            if (one.contains(i))
-                sb.append("1");
-            else if (zero.contains(i))
-                sb.append("0");
+        public Bitset(int size) {
+            this.size = size;
+            for (int i = 0; i < size; i++)
+                zero.add(i);
         }
-        return sb.toString();
+
+        public void fix(int idx) {
+            one.add(idx);
+            zero.remove(idx);
+        }
+
+        public void unfix(int idx) {
+            one.remove(idx);
+            zero.add(idx);
+        }
+
+        public void flip() {
+            Set<Integer> s = one;
+            one = zero;
+            zero = s;
+        }
+
+        public boolean all() {
+            return one.size() == size;
+        }
+
+        public boolean one() {
+            return one.size() >= 1;
+        }
+
+        public int count() {
+            return one.size();
+        }
+
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < size; i++) {
+                if (one.contains(i))
+                    sb.append("1");
+                else if (zero.contains(i))
+                    sb.append("0");
+            }
+            return sb.toString();
+        }
     }
 
     // SOLUTION BEGIN
