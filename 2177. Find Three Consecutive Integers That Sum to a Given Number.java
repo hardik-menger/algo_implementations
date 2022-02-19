463d766b389d900c08a0830b4bce8e260b691ab2
hardik-menger <hardik.menger@cimpress.com>
2022-02-20 00:04:26 +0530

    done

---
+++ 2177. Find Three Consecutive Integers That Sum to a Given Number.java
@@ -0,0 +1,123 @@
+import java.util.*;
+import java.io.*;
+
+class Main2177 {
+    // SOLUTION BEGIN
+    void pre() throws Exception {
+
+    }
+
+    public long[] sumOfThree(long num) {
+        long n = (num - 3) / 3;
+        return (3 * n + 3) == num ? new long[] { n, n + 1, n + 2 } : new long[] {};
+    }
+
+    void solve(int TC) throws Exception {
+        int n = ni();
+    }
+
+    static boolean multipleTC = false;
+    static FastReader in;
+    static PrintWriter out;
+    static Scanner sc;
+
+    void run() throws Exception {
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        sc = new Scanner(System.in);
+        int T = (multipleTC) ? ni() : 1;
+        pre();
+        for (int t = 1; t <= T; t++)
+            solve(t);
+        out.flush();
+        out.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            new Main2177().run();
+        } catch (Exception e) {
+
+        }
+    }
+
+    int bit(long n) {
+        return (n == 0) ? 0 : (1 + bit(n & (n - 1)));
+    }
+
+    void p(Object o) {
+        out.print(o);
+    }
+
+    void pn(Object o) {
+        out.println(o);
+    }
+
+    void pni(Object o) {
+        out.println(o);
+        out.flush();
+    }
+
+    String n() throws Exception {
+        return in.next();
+    }
+
+    String nln() throws Exception {
+        return in.nextLine();
+    }
+
+    int ni() throws Exception {
+        return in.nextInt();
+    }
+
+    long nl() throws Exception {
+        return in.nextLong();
+    }
+
+    double nd() throws Exception {
+        return in.nextDouble();
+    }
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
